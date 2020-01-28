54858d3c5fcec96a48a9f435d62865c7f12ff93f
Maksym Tsedov <maksym.tsedov@solarwinds.com>
2020-01-28 22:00:39 +0200

    Added type guards

--- src/app/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { QuestionBase } from '../Classes/QuestionBase';
 import { FormGroup } from '@angular/forms';
+import { InputQuestion } from '../Classes/question-input';
+import { NumberInputQuestion } from '../Classes/question-number-input';
+import { DropdownQuestion } from '../Classes/question-dropdown';
 
 @Component({
   selector: 'app-question',
@@ -11,5 +14,23 @@ export class DynamicFormQuestionComponent {
   @Input() question: QuestionBase<any>;
   @Input() form: FormGroup;
 
-  get isValid() { return this.form.controls[this.question.key].valid; }
+  get isValid() { return this.form.controls[this.question.key].valid || this.form.valid; }
+
+  get questionInput() { 
+    if (this.question instanceof InputQuestion) {
+      return (this.question as InputQuestion<any>);
+    }
+  }
+
+  get questionNumber() { 
+    if (this.question instanceof NumberInputQuestion) {
+      return (this.question as NumberInputQuestion);
+    }
+  }
+
+  get questionDropdown() { 
+    if (this.question instanceof DropdownQuestion) {
+      return (this.question as DropdownQuestion);
+    }
+  }
 }
